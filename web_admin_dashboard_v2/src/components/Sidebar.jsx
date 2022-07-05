eeeede636bcb0ea9101844b3a0ff7ac3a15481dd
Ulises Bojorquez Ortiz <ulibojorquez@gmail.com>
2022-07-04 22:21:48 -0600

    Context of activeMenu actions

--- web_admin_dashboard_v2/src/components/Sidebar.jsx
+++ web_admin_dashboard_v2/src/components/Sidebar.jsx
@@ -4,9 +4,11 @@ import {SiShopware} from "react-icons/si";
 import {MdOutlineCancel} from "react-icons/md";
 import {TooltipComponent} from "@syncfusion/ej2-react-popups";
 import {links} from "../data/dummy";
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const {activeMenu, setActiveMenu} = useStateContext();
+
   const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
   
@@ -17,12 +19,13 @@ const Sidebar = () => {
         (<>
           {/* Icon and Tooltip component */}
           <div className='flex justify-between'>
-            <Link to="/" onClick={()=>{}} 
+            <Link to="/" onClick={()=>setActiveMenu(false)} 
             className=" flex items-center gap-3 ml-3 mt-4 text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-              <SiShopware className="text-3xl"/><span>Shoppy</span>
+              <SiShopware className="text-3xl"/>
+              <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position='BottomCenter'>
-              <button type='button' onClick={()=>{}} className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
+              <button type='button' onClick={()=>setActiveMenu((prevActiveMenu) => !prevActiveMenu)} className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
--- web_admin_dashboard_v2/src/index.js
+++ web_admin_dashboard_v2/src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
 import "./index.css";
+import {ContextProvider} from "./contexts/ContextProvider";
 
 ReactDOM.render(
-    <App/>,
+    <ContextProvider>
+        <App/>
+    </ContextProvider>,
     document.getElementById("root")
 );
 
