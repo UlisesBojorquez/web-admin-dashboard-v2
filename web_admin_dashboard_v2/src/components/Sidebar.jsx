2253781c7b1f1d734f74a0e0d15d060fde4f062e
Ulises Bojorquez Ortiz <ulibojorquez@gmail.com>
2022-07-06 21:44:53 -0600

    Navbar functionality and complete Sidebar events

--- web_admin_dashboard_v2/src/components/Sidebar.jsx
+++ web_admin_dashboard_v2/src/components/Sidebar.jsx
@@ -7,11 +7,17 @@ import {links} from "../data/dummy";
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const {activeMenu, setActiveMenu, screenSize} = useStateContext();
 
   const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
   
+  const handleCloseSideBar = () =>{
+    if(activeMenu && screenSize <= 900){
+      setActiveMenu(false);
+    }
+  }
+
   return (
     <div className='ml-3 h-screen md:overlow-hidden overflow-auto md:hover:overflow-auto pb-10 '>
       {
@@ -19,7 +25,7 @@ const Sidebar = () => {
         (<>
           {/* Icon and Tooltip component */}
           <div className='flex justify-between'>
-            <Link to="/" onClick={()=>setActiveMenu(false)} 
+            <Link to="/" onClick={handleCloseSideBar} 
             className=" flex items-center gap-3 ml-3 mt-4 text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
               <SiShopware className="text-3xl"/>
               <span>Shoppy</span>
@@ -38,7 +44,7 @@ const Sidebar = () => {
                 <p className='text-gray-400 m-3 mt-4 uppercase'>{item.title}</p>
                 {
                   item.links.map((link) => (
-                    <NavLink to={`/${link.name}`} key={link.name} onClick={()=>{}}
+                    <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSideBar}
                     className={({isActive})=> isActive ? activeLink : normalLink}
                     >
                       {link.icon}
