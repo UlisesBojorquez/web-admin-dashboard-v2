8b83dbdbd8cd6dff55300404c4982b255a639157
Ulises Bojorquez Ortiz <ulibojorquez@gmail.com>
2022-07-08 16:36:01 -0600

    Calendar completed

--- web_admin_dashboard_v2/src/pages/Ecommerce.jsx
+++ web_admin_dashboard_v2/src/pages/Ecommerce.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import {BsCurrencyDollar} from "react-icons/bs";
 import {GoPrimitiveDot} from "react-icons/go";
-import {Stacked,Pie,Button,SparkLine} from "../components";
-import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
+import {Stacked,Button,SparkLine} from "../components";
+import { earningData, SparklineAreaData } from '../data/dummy';
 
 const Ecommerce = () => {
-
-  
 
   return (
     <div className='mt-12'>
